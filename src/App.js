215e51930b452651e909446bb3740074911ea4c1
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2021-11-22 16:11:30 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends Component {
+/* class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    };
+    this.OnWriteHandle = this.OnWriteHandle.bind(this);
+  }
+  OnWriteHandle = (event) => {
+    this.setState({
+      value: event.target.value,
+    });
+    console.log(event.target.value);
+  };
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input
+          type="text"
+          onChange={this.OnWriteHandle}
+          placeholder="Empieza a escribir algo"
+        />
+        <p className="repeater">{this.state.value}</p>
       </div>
     );
   }
 }
 
+export default App; */
+
+const App = () => {
+  const [value, setValue] = useState("");
+  const OnWriteHandle = (event) => {
+     setValue(event.target.value);
+    console.log(event.target.value);
+  };
+  return (
+    <div>
+      <h1>Un repetidor</h1>
+      <input
+        type="text"
+        onChange={OnWriteHandle}
+        placeholder="Empieza a escribir algo"
+      />
+      <p className="repeater">{value}</p>
+    </div>
+  );
+};
 export default App;
